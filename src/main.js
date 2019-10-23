5d0eb496aa8fcc6be5dc87774c12338fc75710a5
sadimfinity <santi-skate@hotmail.com>
2019-10-22 22:33:51 -0500

    Consuming the principal endpoint

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import deleteQuote from './components/deleteQuote.vue'
 import getQuote from './components/getQuote.vue'
 import getImage from './components/getImage.vue'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
