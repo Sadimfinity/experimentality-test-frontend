57c73c92d38384c54d444047827325b593059591
sadimfinity <santi-skate@hotmail.com>
2019-10-22 21:37:20 -0500

    The getQuote view is done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      clipped
+      app
+      color="grey lighten-4"
+    >
+      <v-list
+        dense
+        class="grey lighten-4"
+      >
+        <template>
+          <v-row>
+              <v-list-item-title>
+                <router-link to="/" tag="button">
+                <v-btn
+                  text
+                  large
+                >
+                  {{ Home }}
+                </v-btn>
+                </router-link>
+              </v-list-item-title>
+          </v-row>
+          <v-row>
+              <v-list-item-title>
+                <router-link to="/getQuote" tag="button">
+                <v-btn
+                  text
+                  large
+                >
+                  {{ getRegister }}
+                </v-btn>
+                </router-link>
+              </v-list-item-title>
+          </v-row>
+          <v-row
+            align="center"
+          >
+              <v-list-item-title>
+                <router-link to="/deleteQuote" tag="button">
+                <v-btn
+                  text
+                  large
+                >
+                {{ deleteRegister }}
+                </v-btn>
+                </router-link>
+              </v-list-item-title>
+          </v-row>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <img src="https://www.experimentality.co/wp-content/uploads/2017/04/experimentalitycerebro.png" width="40" height="40">
+        <span>EXPERIMENTALITY</span>
+        <span class="font-weight-light"> TEST</span>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null,
+    deleteRegister: 'Borrar Registro',
+    getRegister: 'Obtener Registro',
+    Home: 'Inicio',
+    items: [
+      { text: '   Obtener frase' },
+      { divider: true },
+      { text: '   Eliminar frase' }
+    ]
+  })
 }
-</script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,38 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueRouter from 'vue-router'
+
+import deleteQuote from './components/deleteQuote.vue'
+import getQuote from './components/getQuote.vue'
+import getImage from './components/getImage.vue'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const routes = [
+  {
+    path: '/',
+    component: getImage
+  },
+  {
+    path: '/getQuote',
+    component: getQuote
+  },
+  {
+    path: '/deleteQuote',
+    component: deleteQuote
+  }
+]
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
